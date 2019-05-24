7fb67250a67aa4811b87d198d86567e665e93f39
Ashley Linares <alinares.u@rrghst.org>
2019-05-24 23:20:54 +0000

    javascript file added

---
+++ script.js
@@ -0,0 +1,10 @@
+/* global $ */
+
+$("#robButton").click(function(){
+    $(".result").html("What did the man say about his day?");
+    console.log("hello");
+});
+
+$("#sadmanButton").click(function(){
+    console.log("hello")
+});
